ebd7ff5250ff0cdb412df4b726a8567567f0d5bf
RZeus <rajbirsinghshienh@gmail.com>
2018-02-26 12:13:06 -0500

    Add project files.

---
+++ NameGuess/Form1.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace NameGuess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list1 = new System.Windows.Forms.ListBox();
+            this.list5 = new System.Windows.Forms.ListBox();
+            this.list4 = new System.Windows.Forms.ListBox();
+            this.list3 = new System.Windows.Forms.ListBox();
+            this.list2 = new System.Windows.Forms.ListBox();
+            this.enter6 = new System.Windows.Forms.ListBox();
+            this.enter5 = new System.Windows.Forms.ListBox();
+            this.enter4 = new System.Windows.Forms.ListBox();
+            this.enter3 = new System.Windows.Forms.ListBox();
+            this.enter2 = new System.Windows.Forms.ListBox();
+            this.enter1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.l6 = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // list1
+            // 
+            this.list1.Enabled = false;
+            this.list1.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list1.FormattingEnabled = true;
+            this.list1.ItemHeight = 33;
+            this.list1.Items.AddRange(new object[] {
+            "A",
+            "F",
+            "K",
+            "P",
+            "U",
+            "Z"});
+            this.list1.Location = new System.Drawing.Point(22, 58);
+            this.list1.Name = "list1";
+            this.list1.Size = new System.Drawing.Size(30, 202);
+            this.list1.TabIndex = 0;
+            // 
+            // list5
+            // 
+            this.list5.Enabled = false;
+            this.list5.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list5.FormattingEnabled = true;
+            this.list5.ItemHeight = 33;
+            this.list5.Items.AddRange(new object[] {
+            "E",
+            "J",
+            "O",
+            "T",
+            "Y"});
+            this.list5.Location = new System.Drawing.Point(309, 58);
+            this.list5.Name = "list5";
+            this.list5.Size = new System.Drawing.Size(30, 202);
+            this.list5.TabIndex = 1;
+            // 
+            // list4
+            // 
+            this.list4.Enabled = false;
+            this.list4.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list4.FormattingEnabled = true;
+            this.list4.ItemHeight = 33;
+            this.list4.Items.AddRange(new object[] {
+            "D",
+            "I",
+            "N",
+            "S",
+            "X"});
+            this.list4.Location = new System.Drawing.Point(234, 58);
+            this.list4.Name = "list4";
+            this.list4.Size = new System.Drawing.Size(30, 202);
+            this.list4.TabIndex = 2;
+            // 
+            // list3
+            // 
+            this.list3.Enabled = false;
+            this.list3.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list3.FormattingEnabled = true;
+            this.list3.ItemHeight = 33;
+            this.list3.Items.AddRange(new object[] {
+            "C",
+            "H",
+            "M",
+            "R",
+            "W"});
+            this.list3.Location = new System.Drawing.Point(161, 58);
+            this.list3.Name = "list3";
+            this.list3.Size = new System.Drawing.Size(30, 202);
+            this.list3.TabIndex = 3;
+            // 
+            // list2
+            // 
+            this.list2.Enabled = false;
+            this.list2.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list2.FormattingEnabled = true;
+            this.list2.ItemHeight = 33;
+            this.list2.Items.AddRange(new object[] {
+            "B",
+            "G",
+            "L",
+            "Q",
+            "V"});
+            this.list2.Location = new System.Drawing.Point(90, 58);
+            this.list2.Name = "list2";
+            this.list2.Size = new System.Drawing.Size(30, 202);
+            this.list2.TabIndex = 4;
+            // 
+            // enter6
+            // 
+            this.enter6.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter6.FormattingEnabled = true;
+            this.enter6.ItemHeight = 33;
+            this.enter6.Location = new System.Drawing.Point(309, 351);
+            this.enter6.Name = "enter6";
+            this.enter6.Size = new System.Drawing.Size(30, 202);
+            this.enter6.TabIndex = 5;
+            this.enter6.Visible = false;
+            // 
+            // enter5
+            // 
+            this.enter5.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter5.FormattingEnabled = true;
+            this.enter5.ItemHeight = 33;
+            this.enter5.Location = new System.Drawing.Point(252, 351);
+            this.enter5.Name = "enter5";
+            this.enter5.Size = new System.Drawing.Size(30, 202);
+            this.enter5.TabIndex = 6;
+            this.enter5.Visible = false;
+            // 
+            // enter4
+            // 
+            this.enter4.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter4.FormattingEnabled = true;
+            this.enter4.ItemHeight = 33;
+            this.enter4.Location = new System.Drawing.Point(194, 351);
+            this.enter4.Name = "enter4";
+            this.enter4.Size = new System.Drawing.Size(30, 202);
+            this.enter4.TabIndex = 7;
+            this.enter4.Visible = false;
+            // 
+            // enter3
+            // 
+            this.enter3.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter3.FormattingEnabled = true;
+            this.enter3.ItemHeight = 33;
+            this.enter3.Location = new System.Drawing.Point(136, 351);
+            this.enter3.Name = "enter3";
+            this.enter3.Size = new System.Drawing.Size(30, 202);
+            this.enter3.TabIndex = 8;
+            this.enter3.Visible = false;
+            // 
+            // enter2
+            // 
+            this.enter2.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter2.FormattingEnabled = true;
+            this.enter2.ItemHeight = 33;
+            this.enter2.Location = new System.Drawing.Point(79, 351);
+            this.enter2.Name = "enter2";
+            this.enter2.Size = new System.Drawing.Size(30, 202);
+            this.enter2.TabIndex = 9;
+            this.enter2.Visible = false;
+            // 
+            // enter1
+            // 
+            this.enter1.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter1.FormattingEnabled = true;
+            this.enter1.ItemHeight = 33;
+            this.enter1.Location = new System.Drawing.Point(22, 350);
+            this.enter1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.enter1.Name = "enter1";
+            this.enter1.Size = new System.Drawing.Size(30, 202);
+            this.enter1.TabIndex = 10;
+            this.enter1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Helvetica-Condensed-Thin", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(136, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(4, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Line 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(288, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Line 5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(217, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Line 4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(146, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Line 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(75, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 19);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Line 2";
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l1.Location = new System.Drawing.Point(28, 318);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(19, 19);
+            this.l1.TabIndex = 21;
+            this.l1.Text = "1";
+            this.l1.Visible = false;
+            // 
+            // l6
+            // 
+            this.l6.AutoSize = true;
+            this.l6.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l6.Location = new System.Drawing.Point(317, 319);
+            this.l6.Name = "l6";
+            this.l6.Size = new System.Drawing.Size(19, 19);
+            this.l6.TabIndex = 26;
+            this.l6.Text = "6";
+            this.l6.Visible = false;
+            // 
+            // l5
+            // 
+            this.l5.AutoSize = true;
+            this.l5.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l5.Location = new System.Drawing.Point(259, 319);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(19, 19);
+            this.l5.TabIndex = 27;
+            this.l5.Text = "5";
+            this.l5.Visible = false;
+            // 
+            // l4
+            // 
+            this.l4.AutoSize = true;
+            this.l4.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l4.Location = new System.Drawing.Point(199, 319);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(19, 19);
+            this.l4.TabIndex = 28;
+            this.l4.Text = "4";
+            this.l4.Visible = false;
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l3.Location = new System.Drawing.Point(142, 319);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(19, 19);
+            this.l3.TabIndex = 29;
+            this.l3.Text = "3";
+            this.l3.Visible = false;
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.l2.Location = new System.Drawing.Point(86, 319);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(19, 19);
+            this.l2.TabIndex = 30;
+            this.l2.Text = "2";
+            this.l2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(358, 309);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.l6);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.enter1);
+            this.Controls.Add(this.enter2);
+            this.Controls.Add(this.enter3);
+            this.Controls.Add(this.enter4);
+            this.Controls.Add(this.enter5);
+            this.Controls.Add(this.enter6);
+            this.Controls.Add(this.list2);
+            this.Controls.Add(this.list3);
+            this.Controls.Add(this.list4);
+            this.Controls.Add(this.list5);
+            this.Controls.Add(this.list1);
+            this.Name = "Form1";
+            this.Text = "Name Guesser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list1;
+        private System.Windows.Forms.ListBox list5;
+        private System.Windows.Forms.ListBox list4;
+        private System.Windows.Forms.ListBox list3;
+        private System.Windows.Forms.ListBox list2;
+        private System.Windows.Forms.ListBox enter6;
+        private System.Windows.Forms.ListBox enter5;
+        private System.Windows.Forms.ListBox enter4;
+        private System.Windows.Forms.ListBox enter3;
+        private System.Windows.Forms.ListBox enter2;
+        private System.Windows.Forms.ListBox enter1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l6;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l2;
+    }
+}
+
---
+++ NameGuess/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameGuess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("How many letters are in your first name?", "Start");
+            if (input != null)
+            {
+                int value = Int32.Parse(input);
+                switch (value)
+                {
+                    case 1:
+                        enter1.Visible = true;
+                        l1.Visible = true;
+                        break;
+                    case 2:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        break;
+                    case 3:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        break;
+                    case 4:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        break;
+                    case 5:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        break;
+                    case 6:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        enter6.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        l6.Visible = true;
+                        break;
+                    case 7:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        enter6.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        l6.Visible = true;
+                        break;
+                    case 8:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        enter6.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        l6.Visible = true;
+                        break;
+                    case 9:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        enter6.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        l6.Visible = true;
+                        break;
+                    case 10:
+                        enter1.Visible = true;
+                        enter2.Visible = true;
+                        enter3.Visible = true;
+                        enter4.Visible = true;
+                        enter5.Visible = true;
+                        enter6.Visible = true;
+                        l1.Visible = true;
+                        l2.Visible = true;
+                        l3.Visible = true;
+                        l4.Visible = true;
+                        l5.Visible = true;
+                        l6.Visible = true;
+                        break;
+                }
+                for (int i = 1; i < value + 1; i++)
+                {
+                    switch (rLetters(i))
+                    {
+                        case 1:
+                            enter1.Items.Add(list1.Items[0]);
+                            enter2.Items.Add(list1.Items[1]);
+                            enter3.Items.Add(list1.Items[2]);
+                            enter4.Items.Add(list1.Items[3]);
+                            enter5.Items.Add(list1.Items[4]);
+                            break;
+                        case 2:
+                            enter1.Items.Add(list2.Items[0]);
+                            enter2.Items.Add(list2.Items[1]);
+                            enter3.Items.Add(list2.Items[2]);
+                            enter4.Items.Add(list2.Items[3]);
+                            enter5.Items.Add(list2.Items[4]);
+                            break;
+                        case 3:
+                            enter1.Items.Add(list3.Items[0]);
+                            enter2.Items.Add(list3.Items[1]);
+                            enter3.Items.Add(list3.Items[2]);
+                            enter4.Items.Add(list3.Items[3]);
+                            enter5.Items.Add(list3.Items[4]);
+                            break;
+                        case 4:
+                            enter1.Items.Add(list4.Items[0]);
+                            enter2.Items.Add(list4.Items[1]);
+                            enter3.Items.Add(list4.Items[2]);
+                            enter4.Items.Add(list4.Items[3]);
+                            enter5.Items.Add(list4.Items[4]);
+                            break;
+                        case 5:
+                            enter1.Items.Add(list5.Items[0]);
+                            enter2.Items.Add(list5.Items[1]);
+                            enter3.Items.Add(list5.Items[2]);
+                            enter4.Items.Add(list5.Items[3]);
+                            enter5.Items.Add(list5.Items[4]);
+                            break;
+                    }
+                }
+            }
+        }
+        private static int rLetters(int i)
+        {
+            string letters = Microsoft.VisualBasic.Interaction.InputBox("In which line has letter number " + i + " ?", "Letter " + i);
+            int letter = 0;
+            if (letters != null)
+            {
+                letter = Int32.Parse(letters);
+            }
+            return letter;
+        }
+    }
+}
